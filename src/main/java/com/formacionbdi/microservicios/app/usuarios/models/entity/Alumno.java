4b1e9c2e289475dee522d4f95e82be1387eccff2
JlFerrete <joseluisferrete@gmail.com>
2020-07-08 10:27:27 +0200

    Changed Alumno Model

--- src/main/java/com/formacionbdi/microservicios/app/usuarios/models/entity/Alumno.java
+++ src/main/java/com/formacionbdi/microservicios/app/usuarios/models/entity/Alumno.java
@@ -9,6 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name="alumnos")
@@ -23,6 +25,7 @@ public class Alumno {
 	private String email;
 	
 	@Column(name = "create_at")
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date createAt;
 	
 	@PrePersist
