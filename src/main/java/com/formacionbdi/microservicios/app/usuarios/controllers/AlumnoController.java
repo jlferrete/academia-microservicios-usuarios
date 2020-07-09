a5fa77a3a9ff00e220f18218de47a1f1041c70ad
JlFerrete <joseluisferrete@gmail.com>
2020-07-09 10:38:21 +0200

    Adding a common library for a generic controller and reusing

--- src/main/java/com/formacionbdi/microservicios/app/usuarios/controllers/AlumnoController.java
+++ src/main/java/com/formacionbdi/microservicios/app/usuarios/controllers/AlumnoController.java
@@ -2,52 +2,21 @@ package com.formacionbdi.microservicios.app.usuarios.controllers;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.microservicios.app.usuarios.models.entity.Alumno;
 import com.formacionbdi.microservicios.app.usuarios.services.AlumnoService;
+import com.formacionbdi.microservicios.commons.controllers.CommonController;
 
 @RestController
-public class AlumnoController {
+public class AlumnoController extends CommonController<Alumno, AlumnoService> {
 
-	@Autowired
-	private AlumnoService service;
-
-	@GetMapping
-	public ResponseEntity<?> listar() {
-		return ResponseEntity.ok().body(service.findAll());
-	}
-	
-	@GetMapping("/{id}")
-	public ResponseEntity<?> ver(@PathVariable Long id) {
-		
-		Optional<Alumno> o = service.findById(id);
-		
-		if(o.isEmpty()) {
-			return ResponseEntity.notFound().build();
-		}
-		
-		return ResponseEntity.ok(o.get());
-	}
-	
-	@PostMapping
-	public ResponseEntity<?> crear(@RequestBody Alumno alumno) {
-		
-		Alumno alumnoDb = service.save(alumno);
-		
-		return ResponseEntity.status(HttpStatus.CREATED).body(alumnoDb);
-	}
-	
 	@PutMapping("/{id}")
 	public ResponseEntity<?> editar(@RequestBody Alumno alumno, @PathVariable Long id){
 		
@@ -66,10 +35,6 @@ public class AlumnoController {
 		
 	}
 	
-	@DeleteMapping("/id")
-	public ResponseEntity<?> eliminar(@PathVariable Long id){
-		service.deleteById(id);
-		return ResponseEntity.noContent().build();
-	}
+	
 	
 }
