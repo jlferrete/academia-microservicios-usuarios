fc74bbc7bc9890170a2af1ba4f5eb859e70e0644
JlFerrete <joseluisferrete@gmail.com>
2020-07-09 10:18:00 +0200

    Adding a common library for a generic service and reusing code

--- src/main/java/com/formacionbdi/microservicios/app/usuarios/services/AlumnoService.java
+++ src/main/java/com/formacionbdi/microservicios/app/usuarios/services/AlumnoService.java
@@ -1,16 +1,11 @@
 package com.formacionbdi.microservicios.app.usuarios.services;
 
-import java.util.Optional;
+
 
 import com.formacionbdi.microservicios.app.usuarios.models.entity.Alumno;
+import com.formacionbdi.microservicios.commons.services.CommonService;
 
-public interface AlumnoService {
+public interface AlumnoService extends CommonService<Alumno> {
 	
-	public Iterable<Alumno> findAll();
 	
-	public Optional<Alumno> findById(Long id);
-	
-	public Alumno save(Alumno alumno);
-	
-	public void deleteById(Long id);
 }
--- src/main/java/com/formacionbdi/microservicios/app/usuarios/services/AlumnoServiceImpl.java
+++ src/main/java/com/formacionbdi/microservicios/app/usuarios/services/AlumnoServiceImpl.java
@@ -1,43 +1,12 @@
 package com.formacionbdi.microservicios.app.usuarios.services;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.formacionbdi.microservicios.app.usuarios.models.entity.Alumno;
 import com.formacionbdi.microservicios.app.usuarios.repository.AlumnoRepository;
+import com.formacionbdi.microservicios.commons.services.CommonServiceImpl;
 
 @Service
-public class AlumnoServiceImpl implements AlumnoService {
-
-	@Autowired
-	private AlumnoRepository repository;
-	
-	@Override
-	@Transactional(readOnly = true)
-	public Iterable<Alumno> findAll() {
-		return repository.findAll();
-	}
-
-	@Override
-	@Transactional(readOnly = true)
-	public Optional<Alumno> findById(Long id) {
-		return repository.findById(id);
-	}
-
-	@Override
-	@Transactional
-	public Alumno save(Alumno alumno) {
-		return repository.save(alumno);
-	}
-
-	@Override
-	@Transactional
-	public void deleteById(Long id) {
-		repository.deleteById(id);
-
-	}
+public class AlumnoServiceImpl extends CommonServiceImpl<Alumno, AlumnoRepository> implements AlumnoService {
 
 }
