7d1447358063e215f49766f36d999484a5a46c49
JlFerrete <joseluisferrete@gmail.com>
2020-07-13 12:35:05 +0200

    Adding validation rules in the entities

--- src/main/java/com/formacionbdi/microservicios/app/usuarios/controllers/AlumnoController.java
+++ src/main/java/com/formacionbdi/microservicios/app/usuarios/controllers/AlumnoController.java
@@ -2,8 +2,11 @@ package com.formacionbdi.microservicios.app.usuarios.controllers;
 
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -19,26 +22,30 @@ import com.formacionbdi.microservicios.commons.controllers.CommonController;
 public class AlumnoController extends CommonController<Alumno, AlumnoService> {
 
 	@PutMapping("/{id}")
-	public ResponseEntity<?> editar(@RequestBody Alumno alumno, @PathVariable Long id){
-		
+	public ResponseEntity<?> editar(@Valid @RequestBody Alumno alumno, BindingResult result, @PathVariable Long id) {
+
+		if (result.hasErrors()) {
+			return this.validar(result);
+		}
+
 		Optional<Alumno> o = service.findById(id);
-		
-		if(o.isEmpty()) {
+
+		if (o.isEmpty()) {
 			return ResponseEntity.notFound().build();
-		} 
-		
+		}
+
 		Alumno alumnoDb = o.get();
 		alumnoDb.setNombre(alumno.getNombre());
 		alumnoDb.setApellidos(alumno.getApellidos());
 		alumnoDb.setEmail(alumno.getEmail());
-		
+
 		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(alumnoDb));
-		
+
 	}
-	
+
 	@GetMapping("/filtrar/{term}")
 	public ResponseEntity<?> filtrar(@PathVariable String term) {
 		return ResponseEntity.ok(service.findByNombreOrApellido(term));
 	}
-	
+
 }
