b52767e805950c9c93bf03af54ccc7b24d9d13a0
JlFerrete <joseluisferrete@gmail.com>
2020-07-14 08:01:14 +0200

    Adding controller handler method to view student's image

--- src/main/java/com/formacionbdi/microservicios/app/usuarios/controllers/AlumnoController.java
+++ src/main/java/com/formacionbdi/microservicios/app/usuarios/controllers/AlumnoController.java
@@ -1,18 +1,24 @@
 package com.formacionbdi.microservicios.app.usuarios.controllers;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.formacionbdi.microservicios.app.usuarios.services.AlumnoService;
 import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
@@ -21,6 +27,24 @@ import com.formacionbdi.microservicios.commons.controllers.CommonController;
 @RestController
 public class AlumnoController extends CommonController<Alumno, AlumnoService> {
 
+	@GetMapping("/uploads/img/{id}")
+	public ResponseEntity<?> verFoto(@PathVariable Long id) {
+		
+		Optional<Alumno> o = service.findById(id);
+
+		if (o.isEmpty() || o.get().getFoto() == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		Resource imagen = new ByteArrayResource(o.get().getFoto());
+		
+		return ResponseEntity.ok()
+				.contentType(MediaType.IMAGE_JPEG)
+				.body(imagen);
+		
+		
+	}
+	
 	@PutMapping("/{id}")
 	public ResponseEntity<?> editar(@Valid @RequestBody Alumno alumno, BindingResult result, @PathVariable Long id) {
 
@@ -47,5 +71,44 @@ public class AlumnoController extends CommonController<Alumno, AlumnoService> {
 	public ResponseEntity<?> filtrar(@PathVariable String term) {
 		return ResponseEntity.ok(service.findByNombreOrApellido(term));
 	}
+
+	@PostMapping("/crear-con-foto")
+	public ResponseEntity<?> crearConFoto(@Valid Alumno alumno, BindingResult result, 
+			@RequestParam MultipartFile archivo ) throws IOException {
+		if(!archivo.isEmpty()) {
+			alumno.setFoto(archivo.getBytes());
+		}
+		
+		return super.crear(alumno, result);
+	}
+	
+	@PutMapping("editar-con-foto/{id}")
+	public ResponseEntity<?> editarConFoto(@Valid Alumno alumno, BindingResult result, @PathVariable Long id, 
+			@RequestParam MultipartFile archivo) throws IOException {
+
+		if (result.hasErrors()) {
+			return this.validar(result);
+		}
+
+		Optional<Alumno> o = service.findById(id);
+
+		if (o.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+
+		Alumno alumnoDb = o.get();
+		alumnoDb.setNombre(alumno.getNombre());
+		alumnoDb.setApellidos(alumno.getApellidos());
+		alumnoDb.setEmail(alumno.getEmail());
+		
+		if(!archivo.isEmpty()) {
+			alumnoDb.setFoto(archivo.getBytes());
+		}
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(alumnoDb));
+
+	}
+	
+	
 
 }
