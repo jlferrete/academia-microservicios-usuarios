6b5b576e7a4aa7ef8c18f1dcf5d5183e9675114f
JlFerrete <joseluisferrete@gmail.com>
2020-07-15 12:51:23 +0200

    Adding upper function in the student repository to filter

--- src/main/java/com/formacionbdi/microservicios/app/usuarios/repository/AlumnoRepository.java
+++ src/main/java/com/formacionbdi/microservicios/app/usuarios/repository/AlumnoRepository.java
@@ -10,6 +10,6 @@ import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
 
 public interface AlumnoRepository extends PagingAndSortingRepository<Alumno, Long> {
 	
-	@Query("select a from Alumno a where a.nombre like %?1% or a.apellido like %?1%")
+	@Query("select a from Alumno a where upper(a.nombre) like upper(concat('%', ?1,'%')) or upper(a.apellido) like upper(concat('%', ?1, '%'))")
 	public List<Alumno> findByNombreOrApellido(String term);	
 }
